b5be7db6afa59990a99bc0c0ac7af6a2c1539e4c
kemal <kemal_seday@hotmail.com>
2020-04-30 11:33:18 +0300

    ASP.NET MVC WEB PROJESİ

---
+++ KurumsalWeb/Controllers/AdminController.cs
@@ -0,0 +1,152 @@
+﻿using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+using System;
+using System.Linq;
+using System.Net.Mail;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Admin
+        [Route("yonetimpaneli")]
+        public ActionResult Index()
+        {
+            ViewBag.BlogSay = db.Blog.Count();
+            ViewBag.KategoriSay = db.Kategori.Count();
+            ViewBag.HizmetSay = db.Hizmet.Count();
+            ViewBag.YorumSay = db.Hizmet.Count();
+            ViewBag.YorumOnay = db.Yorum.Where(x=>x.Onay==false).Count();
+            var sorgu = db.Kategori.ToList();
+            return View(sorgu);
+        }
+        [Route("yonetimpaneli/giris")]
+       public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+           
+            var login = db.Admin.Where(x => x.Eposta == admin.Eposta).SingleOrDefault();
+            if (login.Eposta == admin.Eposta && login.Sifre == Crypto.Hash(admin.Sifre,"MD5"))
+            {
+                Session["adminid"] = login.AdminId;
+                Session["eposta"] = login.Eposta;
+                Session["yetki"] = login.Yetki;
+                return RedirectToAction("Index", "Admin");
+            }
+            ViewBag.Uyari = "Kullanıcı Adı Veya Şifre Hatalı Girdiniz..";
+            return View(admin);
+        }
+        public ActionResult Logout()
+        {
+            Session["adminid"] = null;
+            Session["eposta"]= null;
+            Session.Abandon();
+
+            return RedirectToAction("Login","Admin");
+        }
+        public ActionResult SifremiUnuttum()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SifremiUnuttum(string eposta)
+        {
+            var mail = db.Admin.Where(x=>x.Eposta==eposta).SingleOrDefault();
+            if ( mail!=null)
+            {
+                Random rnd = new Random();
+                int yenisifre = rnd.Next();
+                Admin admin = new Admin();
+                mail.Sifre = Crypto.Hash(Convert.ToString(yenisifre),"MD5");
+                db.SaveChanges();
+
+
+                MailMessage message = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                client.Credentials = new System.Net.NetworkCredential("kemal_seday@hotmail.com", "kemal01seday56");
+                client.Port = 587;
+                client.Host = "smtp.live.com";
+                client.EnableSsl = true;
+                message.To.Add(eposta);
+                message.From = new MailAddress("kemal_seday@hotmail.com");
+                message.Subject = "Şifre Hatırlatma";
+                message.Body = "Şifreniz : " + yenisifre;
+                client.Send(message);
+                ViewBag.Uyari = "Şifreniz Başarıyla Gönderilmiştir";
+
+
+            }
+            else
+            {
+                ViewBag.Uyari = "Hata Oluştu.Tekrar Deneyiniz.";
+            }
+            return View();
+
+           
+        }
+        public ActionResult Adminler()
+        {
+            return View(db.Admin.ToList());
+        }
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Admin admin,string sifre,string eposta)
+        {
+            if(ModelState.IsValid)
+            {
+                admin.Sifre = Crypto.Hash(sifre,"MD5");
+                db.Admin.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+
+            return View(admin);
+        }
+        public ActionResult Edit(int id)
+        {
+            var a = db.Admin.Where(x=>x.AdminId==id).SingleOrDefault();
+            return View(a);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Admin admin,string sifre,string eposta)
+        {
+           if(ModelState.IsValid)
+            {
+                var a = db.Admin.Where(x => x.AdminId == id).SingleOrDefault();
+                a.Sifre = Crypto.Hash(sifre,"MD5");
+                a.Eposta = admin.Eposta;
+                a.Yetki = admin.Yetki;
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+
+            }
+            return View(admin);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var a = db.Admin.Where(x=>x.AdminId==id).SingleOrDefault();
+            if (a != null)
+            {
+                db.Admin.Remove(a);
+                db.SaveChanges();
+                return RedirectToAction("Adminler");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ KurumsalWeb/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+using PagedList;
+using System.Linq;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace KurumsalWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Home
+        [Route("")]
+        [Route("Anasayfa")]
+        
+        public ActionResult Index()
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId);
+           return View();
+        }
+        public ActionResult SliderPartial()
+        {
+
+            return View(db.Slider.ToList().OrderBy(x=>x.SliderId));
+        }
+        public ActionResult HizmetPartial()
+        {
+            return View(db.Hizmet.ToList());
+        }
+        [Route("Hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            return View(db.Hakkimizda.SingleOrDefault()) ;
+        }
+        [Route("Hizmetlerimiz")]
+        public ActionResult Hizmetlerimiz()
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            return View(db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId)) ;
+        }
+        [Route("Iletisim")]
+        public ActionResult Iletisim()
+        {
+           
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            return View(db.Iletisim.SingleOrDefault());
+        }
+        [HttpPost]
+        public ActionResult Iletisim(string adsoyad=null,string email=null,string konu=null,string mesaj=null)
+        {
+            if(adsoyad!=null&&email!=null)
+            {
+                //WebMail.SmtpServer = "smtp.live.com";
+                //WebMail.EnableSsl = true;
+                //WebMail.UserName = "kemal_seday@hotmail.com";
+                //WebMail.Password = "kemal01seday56";
+                //WebMail.SmtpPort = 587;
+                //WebMail.Send("kemal_seday@hotmail.com", konu,email+"-"+ mesaj);
+             
+
+
+                MailMessage message = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                client.Credentials = new System.Net.NetworkCredential("kemal_seday@hotmail.com", "kemal01seday56");
+                client.Port = 587;
+                client.Host = "smtp.live.com";
+                client.EnableSsl = true;
+                message.To.Add("kemal_seday@hotmail.com");
+                message.From = new MailAddress("kemal_seday@hotmail.com");
+                message.Subject = konu;
+                message.Body = "  "+email+"   "+mesaj;
+                client.Send(message);
+                ViewBag.Uyari = "Mesajınız Başarıyla Gönderilmiştir";
+
+
+            }
+            else
+            {
+                ViewBag.Uyari = "Hata Oluştu.Tekrar Deneyiniz.";
+            }
+            return View(db.Iletisim.SingleOrDefault());
+        }
+
+        [Route("BlogPost")]
+        public ActionResult Blog(int Sayfa = 1)
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            return View(db.Blog.Include("Kategori").OrderByDescending(x=>x.BlogId).ToPagedList(Sayfa,5));
+        }
+        [Route("BlogPost/{kategoriad}/{id:int}")]
+        public ActionResult KategoriBlog(int id,int Sayfa=1)
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            var b = db.Blog.Include("Kategori").OrderByDescending(x=>x.BlogId).Where(x=>x.Kategori.KategoriId==id).ToPagedList(Sayfa,5);
+            return View(b);
+        }
+        [Route("BlogPost/{baslik}-{id:int}")]
+        public ActionResult BlogDetay(int id)
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            var b = db.Blog.Include("Kategori").Include("Yorums").Where(x => x.BlogId == id).SingleOrDefault();
+
+            return View(b);
+        }
+        public JsonResult YorumYap(string adsoyad,string eposta,string icerik,int blogid)
+        {
+            if(icerik==null)
+            {
+                return Json(true,JsonRequestBehavior.AllowGet);
+            }
+            db.Yorum.Add(new Yorum {AdSoyad=adsoyad,Eposta=eposta,Icerik=icerik,BlogId=blogid,Onay=false });
+            db.SaveChanges();
+            //Response.Redirect("/Home/BlogDetay/" + blogid);
+
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult BlogKategoriPartial()
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            return PartialView(db.Kategori.Include("Blogs").ToList().OrderBy(x=>x.KategoriAd));
+        }
+        public ActionResult BlogKayitPartial()
+        {
+
+            return PartialView(db.Blog.ToList().OrderByDescending(x=>x.BlogId));
+        }
+
+        public ActionResult FooterPartial()
+        {
+            ViewBag.Kimlik = db.Kimlik.SingleOrDefault();
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x => x.HizmetId);
+            ViewBag.Iletisim = db.Iletisim.SingleOrDefault();
+            ViewBag.Blog = db.Blog.ToList().OrderByDescending(x => x.BlogId);
+
+            return PartialView();
+        }
+    
+
+    }
+}
